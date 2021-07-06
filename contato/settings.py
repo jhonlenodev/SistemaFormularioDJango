6ec5d9893b9dd6eb84674d56884a653cf9d8862d
jhonlenodev <jhonlenofc@gmail.com>
2021-07-06 10:25:10 -0300

    Models Contato

--- contato/settings.py
+++ contato/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import smtplib
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -27,6 +30,11 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+# Emails Admin
+
+ADMINS = (
+    ('Jhon', 'jhonlenoprofissional@gmail.com'),
+)
 
 # Application definition
 
@@ -37,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'core'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +63,7 @@ ROOT_URLCONF = 'contato.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR/'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -103,7 +112,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
@@ -118,8 +127,22 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+EMAIL_BACKEND = 'djangp.core.backends.smtp.EmailBackend'
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'email'
+EMAIL_HOST_PASSWORD = 'senha'
+EMIAL_PORT = 587
--- core/models.py
+++ core/models.py
@@ -1,3 +1,77 @@
+from __future__ import unicode_literals
+import smtplib
+from django import conf
+from django.core.exceptions import MiddlewareNotUsed
 from django.db import models
+from datetime import datetime
+from django.utils.html import format_html
+from smtplib import SMTPAuthenticationError
+from django.core.mail import send_mail
+from django.conf import settings
 
-# Create your models here.
+import sys
+
+class Contato(models.Model):
+    id = models.AutoField(primary_key=True)
+    nome = models.CharField(max_length=150)
+    data = models.DateTimeField(auto_now_add=True)
+    email = models.EmailField()
+    telefone = models.CharField(max_length=25)
+    assunto = models.CharField(max_length=200)
+    mensagem = models.TextField()
+    email_sent = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.nome
+
+    def send_mail(self):
+        message_admin = """
+        Nova Mensagem - Jhon Leno
+        Nome: {0}
+        Email: {1}
+        Telefone: {2}
+        Assunto: {3}
+
+        Mensagem: {4}
+
+        """
+
+        message_admin = message_admin.format(self.nome, self.email, self.telefone,
+         self.assunto, self.mensagem )
+
+        message = """
+        
+        Ola {0}
+        Obrigado por entrar em contato
+        Espero que tenha gostado do projeto.
+        
+        """
+
+        message = message.format(self.nome)
+
+        try:
+            send_mail(
+                'Novo Contato',
+                message_admin,
+                'Jhon Leno FC <jhongamesoficial@gmail.com>',
+                settings.ADMINS,
+                fail_silently=False
+        )
+            send_mail (
+                'Auto Mensagem',
+                message,
+                'Jhon Leno FC <jhongamesoficial@gmail.com>',
+                [self.email],
+                fail_silently=False
+            )
+            self.email_sent = True
+            self.save()
+
+        except smtplib.SMTPAuthenticationError:
+            pass
+def send_confirmation_email(sender, instance, created, **wkargs):
+    if not instance.email_sent:
+        instance.send_mail()
+
+models.signals.post_save.connect(
+    send_confirmation_email, sender=Contato, dispatch_uid='contato.Record')
